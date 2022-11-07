2036a2770a58a4cb622f68377618b509c5472ed7
Victor Coimbra <94536053+VCoimbraB@users.noreply.github.com>
2022-11-07 14:16:56 -0300

    Create 4.4 pacotes.java

---
+++ 4.4 pacotes.java
@@ -0,0 +1,11 @@
+importar  dados . Cadastro . Clientes ;
+pacote  Banco . Contas ;
+
+
+ classe  pública conta {
+    public  static  void  main ( string [] args ) lança  Exception {
+        Cliente  cliente1 = novo  Cliente ();
+        Cliente  cliente2 = novo  Cliente ();
+        Cliente  cliente3 = novo  Cliente ();
+    }
+}
