0333f42af31c86b3df5eefa9160cf1ed89a96835
Victor Coimbra <94536053+VCoimbraB@users.noreply.github.com>
2022-11-07 14:18:15 -0300

    Create 4.5 String.js

---
+++ 4.5 String.js
@@ -0,0 +1,17 @@
+//Estruture três códigos, os quais devem conter uma função de manipulação de string que resulta.
+
+
+//convertendo inteiro em string.
+const  numero  =  250
+const  conversao  =  new  String ( numero )
+consola . log ( conversa )
+
+
+//transformando o texto todo em maiúsculo.
+const  texto  =  'alerta, perigo à frente!'
+consola . log ( texto.toUpperCase ( ) ) _ _
+
+
+//mostra a quantidade de caracteres da palavra digitada.
+var  palavra  =  prompt ( 'Digite uma palavra: ' ) ;
+consola . log ( 'quantidade de caracteres: '  +  palavra . length )
